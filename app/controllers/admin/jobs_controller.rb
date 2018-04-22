c29fd24efd459efc87748a7738c980dcbe350a2e
WANGXUANDING <wangxuanding1@gmail.com>
2018-04-22 23:21:15 +0200

    wage function

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,5 @@
 class Admin::JobsController < ApplicationController
-  before_action :require_is_admin
+  before_fliter :require_is_admin
   def show
        @job = Job.find(params[:id])
      end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,7 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   def index
-    @jobs = Job.all
+    @jobs = Job.all.where(:is_hidden => false)
   end
 
   def new
@@ -44,7 +44,8 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound,
+       :wage_lower_bound, :contact_email)
   end
 
 end
